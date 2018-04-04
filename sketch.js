efd80ff07a6d21603f309174eacaaffd5181a989
Sergio Alonso Pascual <seralpa789@gmail.com>
2018-04-05 00:36:19 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,3 +1,26 @@
+const tam=1
+let puntos=[];
+let p=new Punto(0,0,tam);
+
+function setup() {
+    createCanvas();
+    background(0);
+    puntos[0]=new Punto(400,20,tam*2);
+    puntos[1]=new Punto(100,700,tam*2);
+    puntos[2]=new Punto(700,700,tam*2);
+    for(let i=0;i<puntos.length;i++){
+        puntos[i].show();
+    }   
+}
+
+function draw() {
+    fill(random(0,255),random(0,255),random(0,255))
+    p.show()
+    rnd=Math.floor(random(0,3));
+    p.x+=(puntos[rnd].x-p.x)/2
+    p.y+=(puntos[rnd].y-p.y)/2
+}
+
 function Punto(x,y,tam){
     this.x=x;
     this.y=y;
@@ -6,24 +29,4 @@ function Punto(x,y,tam){
         noStroke();
         ellipse(this.x,this.y,this.tam,this.tam);
     }
-}
-var puntos=[];
-var tam=1
-var p=new Punto(0,0,tam);
-function setup() {
-    createCanvas(800,800);
-    background(51);
-    puntos[0]=new Punto(400,20,tam*2);
-    puntos[1]=new Punto(100,700,tam*2);
-    puntos[2]=new Punto(700,700,tam*2);
-    for(var i=0;i<puntos.length;i++){
-        puntos[i].show();
-    }   
-}
-function draw() {
-    fill(random(0,255),random(0,255),random(0,255))
-    p.show()
-    rnd=Math.floor(random(0,3));
-    p.x+=(puntos[rnd].x-p.x)/2
-    p.y+=(puntos[rnd].y-p.y)/2
 }
